64211d874a4571015ed74f73aab57bbdecb714c4
Student <student@ubuntu.(none)>
2012-10-03 16:38:19 +0200

    lite ändringar i css och index och har lagt till att man skriver ut event

--- client/template_helpers.js
+++ client/template_helpers.js
@@ -5,3 +5,6 @@ Template.eventbooking.category = function () {
 Template.eventbooking.numberOfEvents = function () {
 	return Events.find({category: this.name}).count();
 };
+Template.eventbooking.event = function () {
+	return Events.find({});
+};
