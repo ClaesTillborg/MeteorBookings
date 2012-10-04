634093a728969409c4d485950f08e55852ab25de
hanserikb <hanserikb@gmail.com>
2012-10-04 15:51:21 +0200

    Ändra events lite

--- client/event_handler.js
+++ client/event_handler.js
@@ -1,5 +1,13 @@
 Template.eventbooking.events({
   	'click .category': function () {
-    Session.set("selectedCategory", this.name);
+      Router.setCategory(this.name);
+      //Session.set("selectedCategory", this.name);
+  }
+});
+
+Template.eventlist.events({
+  'click .event': function () {
+    Router.setEvent(this._id);
+    //Session.set("selectedEvent", );
   }
 });
