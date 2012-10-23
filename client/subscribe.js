e261e7ac241f2d950282b2bc9be194b73ae95a54
hanserikb <hanserikb@gmail.com>
2012-10-23 11:01:03 +0200

    Länkar, Routes, låsningstid

--- client/subscribe.js
+++ client/subscribe.js
@@ -3,3 +3,10 @@
  */
 Meteor.subscribe("events");
 Meteor.subscribe("categories");
+Meteor.subscribe("userData");
+
+//Meteor.subscribe("eventCount");
+/*
+Meteor.autosubscribe(function () {
+  Meteor.subscribe("events", Session.get("selectedCategory"));
+});*/
